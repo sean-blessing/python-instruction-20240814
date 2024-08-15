8b6efa7a85466f355f522f6a8dd2cb09c8f1df66
Sean Blessing <snblessing@gmail.com>
2024-08-15 13:59:19 -0400

    file processing

--- 04-file-io.py
+++ 04-file-io.py
@@ -2,11 +2,30 @@
 
 # p.103 - reading_files.py examples
 # simple open and close
+FILE_NAME = './files/test-file.txt'
+file_in = open(FILE_NAME)
+# reading in the file
+file_in.close()
 
 # open a file, iterate over the lines, strip whitespace, print line
 # using 'with' eliminates need for 'close'
+with open(FILE_NAME) as file_in:
+    # iterate over the lines in the file
+    for raw_line in file_in:
+        #print(f'raw_line: {raw_line}')
+        # remove whitespace
+        line = raw_line.rstrip()
+        print(f'line: {line}')
+print("=" * 40)
 
 # read entire file into one string
+with open(FILE_NAME) as file_in:
+    contents = file_in.read()
+    print("NORMAL: ")
+    print(contents)
+    print("RAW:")
+    print(repr(contents))
+print("=" * 40)
 
 # read all lines into an array
 
